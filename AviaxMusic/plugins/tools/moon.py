6949842e91c270e93ed4fef420eac52d59744244
villain-ji <adirajput12309@gmail.com>
2025-01-12 22:41:25 +0530

    Update moon.py

--- AviaxMusic/plugins/tools/moon.py
+++ AviaxMusic/plugins/tools/moon.py
@@ -94,7 +94,7 @@ async def brah3(app: app, message: Message):
     x = 0
     for user in message.video_chat_members_invited.users:
         try:
-            text += f" {user.first_name}) "
+            text += f" {message.to_user.first_name} "
             x += 1
         except Exception:
             pass

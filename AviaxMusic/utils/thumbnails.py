e5e63074a556fc81a6b42f176d45e47a06eb713c
villain-ji <adirajput12309@gmail.com>
2025-01-12 14:57:47 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -130,9 +130,9 @@ async def gen_qthumb(vidid):
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("assets/font2.ttf", 30)
-        font = ImageFont.truetype("assets/font.ttf", 30)
-        title_font = ImageFont.truetype("assets/font3.ttf", 45)
+        arial = ImageFont.truetype("AviaxMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("AviaxMusic/assets/font.ttf", 30)
+        title_font = ImageFont.truetype("AviaxMusic/assets/font3.ttf", 45)
 
         circle_thumbnail = crop_center_circle(youtube, 400, 20)
         circle_thumbnail = circle_thumbnail.resize((400, 400))
@@ -242,9 +242,9 @@ async def gen_thumb(videoid):
     enhancer = ImageEnhance.Brightness(background)
     background = enhancer.enhance(0.6)
     draw = ImageDraw.Draw(background)
-    arial = ImageFont.truetype("assets/font2.ttf", 30)
-    font = ImageFont.truetype("assets/font.ttf", 30)
-    title_font = ImageFont.truetype("assets/font3.ttf", 45)
+    arial = ImageFont.truetype("AviaxMusic/assets/font2.ttf", 30)
+    font = ImageFont.truetype("AviaxMusic/assets/font.ttf", 30)
+    title_font = ImageFont.truetype("AviaxMusic/assets/font3.ttf", 45)
 
     circle_thumbnail = crop_center_circle(youtube, 400, 20)
     circle_thumbnail = circle_thumbnail.resize((400, 400))
@@ -304,3 +304,4 @@ async def gen_thumb(videoid):
         pass
     background.save(f"cache/{videoid}_v4.png")
     return f"cache/{videoid}_v4.png"
+    

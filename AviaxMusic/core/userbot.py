955f83f70636477040f73f452f03704c40f40d71
villain-ji <adirajput12309@gmail.com>
2025-01-12 14:07:37 +0530

    Update userbot.py

--- AviaxMusic/core/userbot.py
+++ AviaxMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AviaxOfficial")
-                await self.one.join_chat("AviaxSupport")
+                await self.one.join_chat("untold_coder")
+                await self.one.join_chat("untold_coder")
             except:
                 pass
             assistants.append(1)

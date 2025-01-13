db5246b9ce513ba9da56cdd7a9f1186a42e37886
villain-ji <adirajput12309@gmail.com>
2025-01-13 08:31:05 +0530

    Update vctool.py

--- AviaxMusic/plugins/tools/vctool.py
+++ AviaxMusic/plugins/tools/vctool.py
@@ -6,27 +6,11 @@ from config import OWNER_ID
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-       await msg.reply("ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ")
+       await msg.reply("💫𝙑𝙊𝙄𝘾𝙀 𝘾𝙃𝘼𝙏 𝙎𝙏𝘼𝙍𝙏𝙀𝘿💫")
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-       await msg.reply("**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
-
-# invite members on vc
-@app.on_message(filters.video_chat_members_invited)
-async def brah3(app :app, message:Message):
-           text = f"{message.from_user.mention} ɪɴᴠɪᴛᴇᴅ "
-           x = 0
-           for user in message.video_chat_members_invited.users:
-             try:
-               text += f"[{user.first_name}](tg://user?id={user.id}) "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text} 😉")
-           except:
-             pass
+       await msg.reply("⭐𝑽𝑶𝑰𝑪𝑬 𝑪𝑯𝑨𝑻 𝑬𝑵𝑫𝑬𝑫⭐")
 
 
 @app.on_message(filters.command("math", prefixes="/"))
